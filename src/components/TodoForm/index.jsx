bb7fba4ea73af880da31d66d2b20a1167dec7228
KiKiYang97 <dunka97@163.com>
2020-08-04 16:05:57 +0800

    [TodoList][KiKi,James] feature: add TodoForm

---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+class TodoForm extends React.Component {
+
+    handleChange = (e) => {
+        // let newlength = Number(e.target.value);
+        // // store.dispatch({type: INIT, length})
+        // this.props.init(newlength)
+        // this.setState({
+        //     length : newlength
+        // });
+    }
+    handleSubmit=(e)=>{
+        
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="add" />
+          </form>
+        );
+    }
+}
+export default TodoForm;
